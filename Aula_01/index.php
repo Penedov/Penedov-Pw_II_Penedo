11411924f83ef8a3ba89864a283814dfd7094725
Vinicius Penedo <Viniciuspenedo2003@gmail.com>
2024-08-03 10:58:08 -0300

    Introdução ao PHP e Xampp, conceitos sobre MVC e HTTPS

---
+++ Aula_01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP test</title>
+    <link rel="website icon" type="png" href="image/icon_php.png">
+</head>
+<body>
+    <?php ?>
+<?php echo '<h1>Hello World</h1>'; ?>
+</body>
+</html>
